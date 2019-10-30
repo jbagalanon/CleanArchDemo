53c670f852380180f816ac09c4d8fd6e1bdbe5f9
Joemar Bagalanon <jbagalanon@hotmail.com>
2019-10-30 15:43:00 +0800

    Added Ioc and dependency injection

--- CleanArch/ClearnArch.Mvc/Startup.cs
+++ CleanArch/ClearnArch.Mvc/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -51,6 +52,7 @@ namespace CleanArch.Mvc
             
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+        RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,6 +82,11 @@ namespace CleanArch.Mvc
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
