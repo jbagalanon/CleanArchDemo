eab9f218d8ff1cbb3cc7afcb7e8448d7df4d4ac5
Joemar Bagalanon <jbagalanon@hotmail.com>
2019-10-30 09:55:05 +0800

    Separate Database Identity

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public  DbSet<Course>Courses { get; set; }
+    }
+
+}
--- CleanArch/ClearnArch.Mvc/Startup.cs
+++ CleanArch/ClearnArch.Mvc/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -42,7 +43,14 @@ namespace CleanArch.Mvc
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<UniversityDBContext>(options => { 
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+
+        });
+            
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
